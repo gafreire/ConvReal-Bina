dd3cd7ce03a56d496cd6e8a2330f3409d3dee9b0
gabriel freire <gfreirefumes@gmail.com>
2022-10-11 12:28:52 -0300

    Transformação do número em binários

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print("Hello World")
+# Entrada de dados 
+
+n = float(input("Entre com um número decimal: "))
+y = 0
+arm = []
+x = -10
+cont = 0
+
+# Validação da entrada de dados
+if (n > 0) and (n < 1) :
+    #print("Nome do projeto: Conversor de números reais para binário")
+    #print("Integrantes do grupo:\n", "Gabriel Freire\n", "Giovanna Nacarato\n", "Nathan Vieira\n", "Rafaela Moura\n")
+    while(cont < 5):
+        n *= 2
+        arm.append(int(n))
+        if (n > 1):
+           n -= 1 
+        cont += 1
+    print(arm)
+
+else:
+    print("ERRO, dados incorretos")
